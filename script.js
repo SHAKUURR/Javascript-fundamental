b8f9e39624f367c4764962726ff9759e2bbff5d7
iamshakur <salmanshakur7@gmail.com>
2023-01-22 14:19:13 -0800

    fixed bug

--- script.js
+++ script.js
@@ -83,121 +83,121 @@
 // console.log(secondResult + "🤩");
 
 // //MY PERSONAL PROJECT!-----------------------------------------------------------------------------
-// const userName = prompt('Tell me your name! I will say something nice about you');
-// let resultText;
-// if (userName[0] === 'A') {
-//   resultText = `Dear ${userName}, you are a generous person.`
-// } else if (userName[0] === 'B') {
-//   resultText = `Dear ${userName}, you are a kind-hearted person.`
-// } else if (userName[0] === 'C') {
-//   resultText = `Dear ${userName}, you are a lovely person.`
-// } else if (userName[0] === 'D') {
-//   resultText = `Dear  ${userName}, you are a brave person.`
-// } else if (userName[0] === 'E') {
-//   resultText = `Dear ${userName}, you are an amazing person.`
-// } else if (userName[0] === 'F') {
-//   resultText = `Dear ${userName}, your warm personality lights up any room you enter.`
-// } else if (userName[0] === 'G') {
-//   resultText = `Dear ${userName}, you are a superstar.`
-// } else if (userName[0] === 'H') {
-//   resultText = `Dear ${userName}, you are a genius.`
-// } else if (userName[0] === 'I') {
-//   resultText = `Dear ${userName}, you are a caring person.`
-// } else if (userName[0] === 'J') {
-//   resultText = `Dear ${userName}, you are an unselfish person.`
-// } else if (userName[0] === 'K') {
-//   resultText = `Dear ${userName}, you are a thoughtful person.`
-// } else if (userName[0] === 'L') {
-//   resultText = `Dear ${userName}, you are a big-souled person.`
-// } else if (userName[0] === 'M') {
-//   resultText = `Dear ${userName}, you are a Liberal-minded person.`
-// } else if (userName[0] === 'N') {
-//   resultText = `Dear ${userName}, you are a Philanthropical person.`
-// } else if (userName[0] === 'O') {
-//   resultText = `Dear ${userName}, you are an open-minded person.`
-// } else if (userName[0] === 'P') {
-//   resultText = `Dear ${userName}, you are a giving-hearted person.`
-// } else if (userName[0] === 'Q') {
-//   resultText = `Dear ${userName}, you are an altruistic person.`
-// } else if (userName[0] === 'R') {
-//   resultText = `Dear ${userName}, you are a giving person.`
-// } else if (userName[0] === 'S') {
-//   resultText = `Dear ${userName}, you are a magnamimous person.`
-// } else if (userName[0] === 'T') {
-//   resultText = `Dear ${userName}, you are a helpful person.`
-// } else if (userName[0] === 'U') {
-//   resultText = `Dear ${userName}, you are a good-natured person.`
-// } else if (userName[0] === 'V') {
-//   resultText = `Dear ${userName}, you are an unselfish person.`
-// } else if (userName[0] === 'W') {
-//   resultText = `Dear ${userName}, you are a liberal-minded person.`
-// } else if (userName[0] === 'X') {
-//   resultText = `Dear ${userName}, you are a charitable person.`
-// } else if (userName[0] === 'Y') {
-//   resultText = `Dear ${userName}, you are a Large-hearted person person.`
-// } else if (userName[0] === 'Z') {
-//   resultText = `Dear ${userName}, you are a thoughtful and amazing person.`
-// } else if (userName[0] === 'a') {
-//   resultText = `Dear ${userName}, you are a generous person.`
-// } else if (userName[0] === 'b') {
-//   resultText = `Dear ${userName}, you are a kind-hearted person.`
-// } else if (userName[0] === 'c') {
-//   resultText = `Dear ${userName}, you are a lovely person.`
-// } else if (userName[0] === 'd') {
-//   resultText = `Dear  ${userName}, you are a brave person.`
-// } else if (userName[0] === 'e') {
-//   resultText = `Dear ${userName}, you are an amazing person.`
-// } else if (userName[0] === 'f') {
-//   resultText = `Dear ${userName}, your warm personality lights up any room you enter.`
-// } else if (userName[0] === 'g') {
-//   resultText = `Dear ${userName}, you are a superstar.`
-// } else if (userName[0] === 'h') {
-//   resultText = `Dear ${userName}, you are a genius.`
-// } else if (userName[0] === 'i') {
-//   resultText = `Dear ${userName}, you are a caring person.`
-// } else if (userName[0] === 'j') {
-//   resultText = `Dear ${userName}, you are an unselfish person.`
-// } else if (userName[0] === 'k') {
-//   resultText = `Dear ${userName}, you are a thoughtful person.`
-// } else if (userName[0] === 'l') {
-//   resultText = `Dear ${userName}, you are a big-souled person.`
-// } else if (userName[0] === 'm') {
-//   resultText = `Dear ${userName}, you are a Liberal-minded person.`
-// } else if (userName[0] === 'n') {
-//   resultText = `Dear ${userName}, you are a Philanthropical person.`
-// } else if (userName[0] === 'o') {
-//   resultText = `Dear ${userName}, you are an open-minded person.`
-// } else if (userName[0] === 'p') {
-//   resultText = `Dear ${userName}, you are a giving-hearted person.`
-// } else if (userName[0] === 'q') {
-//   resultText = `Dear ${userName}, you are an altruistic person.`
-// } else if (userName[0] === 'r') {
-//   resultText = `Dear ${userName}, you are a giving person.`
-// } else if (userName[0] === 's') {
-//   resultText = `Dear ${userName}, you are a magnamimous person.`
-// } else if (userName[0] === 't') {
-//   resultText = `Dear ${userName}, you are a helpful person.`
-// } else if (userName[0] === 'u') {
-//   resultText = `Dear ${userName}, you are a good-natured person.`
-// } else if (userName[0] === 'v') {
-//   resultText = `Dear ${userName}, you are an unselfish person.`
-// } else if (userName[0] === 'w') {
-//   resultText = `Dear ${userName}, you are a liberal-minded person.`
-// } else if (userName[0] === 'x') {
-//   resultText = `Dear ${userName}, you are a charitable person.`
-// } else if (userName[0] === 'y') {
-//   resultText = `Dear ${userName}, you are a Large-hearted person person.`
-// } else if (userName[0] === 'z') {
-//   resultText = `Dear ${userName}, you are a thoughtful and amazing person.`
-// } else {
-//   resultText = `Oops! ${userName} is not a name. 😌😌
-// Kindly input your real name...`
-// }
+const userName = prompt('Tell me your name! I will say something nice about you');
+let resultText;
+if (userName[0] === 'A') {
+  resultText = `Dear ${userName}, you are a generous person.`
+} else if (userName[0] === 'B') {
+  resultText = `Dear ${userName}, you are a kind-hearted person.`
+} else if (userName[0] === 'C') {
+  resultText = `Dear ${userName}, you are a lovely person.`
+} else if (userName[0] === 'D') {
+  resultText = `Dear  ${userName}, you are a brave person.`
+} else if (userName[0] === 'E') {
+  resultText = `Dear ${userName}, you are an amazing person.`
+} else if (userName[0] === 'F') {
+  resultText = `Dear ${userName}, your warm personality lights up any room you enter.`
+} else if (userName[0] === 'G') {
+  resultText = `Dear ${userName}, you are a superstar.`
+} else if (userName[0] === 'H') {
+  resultText = `Dear ${userName}, you are a genius.`
+} else if (userName[0] === 'I') {
+  resultText = `Dear ${userName}, you are a caring person.`
+} else if (userName[0] === 'J') {
+  resultText = `Dear ${userName}, you are an unselfish person.`
+} else if (userName[0] === 'K') {
+  resultText = `Dear ${userName}, you are a thoughtful person.`
+} else if (userName[0] === 'L') {
+  resultText = `Dear ${userName}, you are a big-souled person.`
+} else if (userName[0] === 'M') {
+  resultText = `Dear ${userName}, you are a Liberal-minded person.`
+} else if (userName[0] === 'N') {
+  resultText = `Dear ${userName}, you are a Philanthropical person.`
+} else if (userName[0] === 'O') {
+  resultText = `Dear ${userName}, you are an open-minded person.`
+} else if (userName[0] === 'P') {
+  resultText = `Dear ${userName}, you are a giving-hearted person.`
+} else if (userName[0] === 'Q') {
+  resultText = `Dear ${userName}, you are an altruistic person.`
+} else if (userName[0] === 'R') {
+  resultText = `Dear ${userName}, you are a giving person.`
+} else if (userName[0] === 'S') {
+  resultText = `Dear ${userName}, you are a magnamimous person.`
+} else if (userName[0] === 'T') {
+  resultText = `Dear ${userName}, you are a helpful person.`
+} else if (userName[0] === 'U') {
+  resultText = `Dear ${userName}, you are a good-natured person.`
+} else if (userName[0] === 'V') {
+  resultText = `Dear ${userName}, you are an unselfish person.`
+} else if (userName[0] === 'W') {
+  resultText = `Dear ${userName}, you are a liberal-minded person.`
+} else if (userName[0] === 'X') {
+  resultText = `Dear ${userName}, you are a charitable person.`
+} else if (userName[0] === 'Y') {
+  resultText = `Dear ${userName}, you are a Large-hearted person person.`
+} else if (userName[0] === 'Z') {
+  resultText = `Dear ${userName}, you are a thoughtful and amazing person.`
+} else if (userName[0] === 'a') {
+  resultText = `Dear ${userName}, you are a generous person.`
+} else if (userName[0] === 'b') {
+  resultText = `Dear ${userName}, you are a kind-hearted person.`
+} else if (userName[0] === 'c') {
+  resultText = `Dear ${userName}, you are a lovely person.`
+} else if (userName[0] === 'd') {
+  resultText = `Dear  ${userName}, you are a brave person.`
+} else if (userName[0] === 'e') {
+  resultText = `Dear ${userName}, you are an amazing person.`
+} else if (userName[0] === 'f') {
+  resultText = `Dear ${userName}, your warm personality lights up any room you enter.`
+} else if (userName[0] === 'g') {
+  resultText = `Dear ${userName}, you are a superstar.`
+} else if (userName[0] === 'h') {
+  resultText = `Dear ${userName}, you are a genius.`
+} else if (userName[0] === 'i') {
+  resultText = `Dear ${userName}, you are a caring person.`
+} else if (userName[0] === 'j') {
+  resultText = `Dear ${userName}, you are an unselfish person.`
+} else if (userName[0] === 'k') {
+  resultText = `Dear ${userName}, you are a thoughtful person.`
+} else if (userName[0] === 'l') {
+  resultText = `Dear ${userName}, you are a big-souled person.`
+} else if (userName[0] === 'm') {
+  resultText = `Dear ${userName}, you are a Liberal-minded person.`
+} else if (userName[0] === 'n') {
+  resultText = `Dear ${userName}, you are a Philanthropical person.`
+} else if (userName[0] === 'o') {
+  resultText = `Dear ${userName}, you are an open-minded person.`
+} else if (userName[0] === 'p') {
+  resultText = `Dear ${userName}, you are a giving-hearted person.`
+} else if (userName[0] === 'q') {
+  resultText = `Dear ${userName}, you are an altruistic person.`
+} else if (userName[0] === 'r') {
+  resultText = `Dear ${userName}, you are a giving person.`
+} else if (userName[0] === 's') {
+  resultText = `Dear ${userName}, you are a magnamimous person.`
+} else if (userName[0] === 't') {
+  resultText = `Dear ${userName}, you are a helpful person.`
+} else if (userName[0] === 'u') {
+  resultText = `Dear ${userName}, you are a good-natured person.`
+} else if (userName[0] === 'v') {
+  resultText = `Dear ${userName}, you are an unselfish person.`
+} else if (userName[0] === 'w') {
+  resultText = `Dear ${userName}, you are a liberal-minded person.`
+} else if (userName[0] === 'x') {
+  resultText = `Dear ${userName}, you are a charitable person.`
+} else if (userName[0] === 'y') {
+  resultText = `Dear ${userName}, you are a Large-hearted person person.`
+} else if (userName[0] === 'z') {
+  resultText = `Dear ${userName}, you are a thoughtful and amazing person.`
+} else {
+  resultText = `Oops! ${userName} is not a name. 😌😌
+Kindly input your real name...`
+}
 
-// console.log(resultText);
+console.log(resultText);
 
-//Better way of doing it!----------------------------------------------
-// let input = prompt('Enter your name');
+// //Better way of doing it!----------------------------------------------
+//   let input = prompt('Enter your name');
 // if (isNaN(input)) {
 //   console.log("Great name");
 // } else {
@@ -233,15 +233,15 @@
 // }
 
 // //CODING CHALLENGE--3(BONUS-2)
-const scoreDolphins = (97 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 106) / 3;
-console.log(scoreDolphins, scoreKoalas);
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-  console.log('Dolphin wins the trophy! 🎉🎉');
-} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
-  console.log('Koalas wins the trophy! 🎉🎉')
-} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreDolphins >= 100) {
-  console.log('Good job! Dolphins and Koalas have the same score...')
-} else {
-  console.log('None meet the minimum score!');
-}
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 106) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log('Dolphin wins the trophy! 🎉🎉');
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//   console.log('Koalas wins the trophy! 🎉🎉')
+// } else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreDolphins >= 100) {
+//   console.log('Good job! Dolphins and Koalas have the same score...')
+// } else {
+//   console.log('None meet the minimum score!');
+// }
